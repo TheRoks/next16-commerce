3c48b90711ccc4ba8d4063d2fd33ce9e5b52108a
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-29 22:12:57 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  description: 'Next.js App Router "use cache" demo',
+  title: 'Next.js App Router  "use cache" demo',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
