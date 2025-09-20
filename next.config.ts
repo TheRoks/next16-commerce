4237fa6f2d489e9151b42f36ea9d34ace0b5542a
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-20 16:47:33 +0200

    disable cachecoomponents

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
+    cacheComponents: true,
     inlineCss: true,
     reactCompiler: true,
     staleTimes: {
