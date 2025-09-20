db5a29e7c3532e6ed0a1c9e8de5f7a99ec53c83b
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-20 23:13:24 +0200

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Geist } from 'next/font/google';
 import Footer from '@/components/Footer';
 import { BoundaryProvider } from '@/components/internal/BoundaryProvider';
@@ -20,6 +21,7 @@ export default async function RootLayout({ children }: LayoutProps<'/'>) {
           <div className="flex min-h-screen flex-col">{children}</div>
           <Footer />
         </BoundaryProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
