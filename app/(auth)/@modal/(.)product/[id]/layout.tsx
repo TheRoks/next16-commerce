f0aa23a929b21f171b46924eb38b017e7657d998
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-21 10:14:48 +0200

    Dont access params in modal layout

--- app/(auth)/@modal/(.)product/[id]/layout.tsx
+++ app/(auth)/@modal/(.)product/[id]/layout.tsx
@@ -1,20 +1,9 @@
 import Modal from '@/components/ui/Modal';
 
-export default async function ProductModalLayout({ params, children }: LayoutProps<'/product/[id]'>) {
-  const { id } = await params;
-  const productId = Number(id);
-
+export default async function ProductModalLayout({ children }: LayoutProps<'/product/[id]'>) {
   return (
     <Modal goBackOnClose openModal={true} title="Quick Preview">
       {children}
-      <div className="mt-6 flex justify-center">
-        <a
-          className="text-primary hover:text-primary-dark inline-flex items-center text-sm font-medium"
-          href={`/product/${productId}`}
-        >
-          {'View product details ->'}
-        </a>
-      </div>
     </Modal>
   );
 }
--- app/(auth)/@modal/(.)product/[id]/loading.tsx
+++ app/(auth)/@modal/(.)product/[id]/loading.tsx
@@ -1,5 +1,12 @@
 import { ProductSkeleton } from '@/features/product/components/Product';
 
 export default async function ProductModal() {
-  return <ProductSkeleton />;
+  return (
+    <>
+      <ProductSkeleton />
+      <div className="mt-6 flex justify-center">
+        <div className="skeleton-animation h-5 w-40 rounded" />
+      </div>
+    </>
+  );
 }
