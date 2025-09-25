30a23385d809439a25da19e2197add02cdb30e86
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-25 17:25:21 +0200

    Test ssg

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-
 import Link from 'next/link';
 import React, { Suspense } from 'react';
 import Categories from '@/features/category/components/Categories';
