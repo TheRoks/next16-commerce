1f52b20b8662ae13b7185369a1dd0fd9aa2400f8
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-29 19:00:16 +0200

    Update

--- app/user/unauthorized.tsx
+++ app/user/unauthorized.tsx
@@ -2,7 +2,7 @@ import { Lock } from 'lucide-react';
 import React, { Suspense } from 'react';
 import LoginButton from '@/features/auth/components/LoginButton';
 
-export default async function Unauthorized() {
+export default function Unauthorized() {
   return (
     <div className="flex flex-col items-center justify-center rounded-lg p-8 text-center">
       <Lock className="text-primary mb-4 size-16" />
